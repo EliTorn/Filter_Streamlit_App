5dbc8e670b96cf87a5f32676bb7b4ed140bde449
EliTorn <103903954+EliTorn@users.noreply.github.com>
2023-10-31 21:12:06 +0200

    add the download button

--- file_uploader.py
+++ file_uploader.py
@@ -1,6 +1,20 @@
+from io import BytesIO
 import streamlit as st
 from PIL import Image
 from filter import Filter
+
+
+def download_image():
+    # Convert PIL Image to bytes
+    modified_image_bytes = BytesIO()
+    modified_image.save(modified_image_bytes, format="JPEG")  # Change format as needed
+
+    # Use st.download_button from the st object
+    st.download_button(label="Download image",
+                       data=modified_image_bytes.getvalue(),
+                       file_name="modified.jpeg",
+                       mime="image/jpeg")
+
 
 # Page title
 st.title("Image Uploader")
@@ -8,7 +22,6 @@ st.title("Image Uploader")
 # File uploader widget
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Check if an image is uploaded
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
@@ -19,6 +32,7 @@ if uploaded_file is not None:
 
     if modified_image is not None:
         col2.image(modified_image, caption='Modified Image')
+        download_image()
 
 else:
     st.warning("Please upload an image.")
